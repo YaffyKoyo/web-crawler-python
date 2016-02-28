1d695fd2fc25a0eed2e96f98e4c1dfc017259801
YaffyKoyo <qyaffy@gmail.com>
2016-02-28 22:40:56 +0000

    reorga

---
+++ webcrawler/html_downloader.py
@@ -0,0 +1,26 @@
+'''
+Created on 24 Feb 2016
+
+@author: yaffy
+'''
+import urllib2
+
+
+class HtmlDownloader(object):
+    
+    
+    def download(self,url):
+        if url is None:
+            return None
+        
+        response = urllib2.urlopen(url)
+        
+        if response.getcode()!= 200:
+            return None
+        
+        return response.read()
+    
+    
+
+
+
